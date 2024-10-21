e2324ea3ae38ff2082e0778c18074fe776f70cc2
lovrodukic <lovrodukic@gmail.com>
2024-10-20 20:44:12 -0700

    Create bucket only if it does not exist

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,16 @@
+# Set AWS provider
 provider "aws" {
   region = var.region
 }
 
+# Attempt to fetch details of an existing bucket
+data "aws_s3_bucket" "devsecops-llm-pipeline" {
+  bucket = var.bucket_name
+}
+
+# Create a bucket only if it does not exist
 resource "aws_s3_bucket" "devsecops-llm-pipeline" {
+  count  = length(data.aws_s3_bucket.devsecops-llm-pipeline.*.id) > 0 ? 0 : 1
   bucket = var.bucket_name
 
   tags = {
