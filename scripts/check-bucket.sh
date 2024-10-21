6222ccc07c7f53c246c7e2ed47d45c06eda720a1
lovrodukic <lovrodukic@gmail.com>
2024-10-20 21:59:15 -0700

    Testing scripts/

---
+++ scripts/check-bucket.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUCKET_NAME=$1
+
+if aws s3api head-bucket --bucket $BUCKET_NAME 2>/dev/null; then
+  echo "Bucket exists."
+  echo "BUCKET_EXISTS=true" >> $GITHUB_ENV
+else
+  echo "Bucket does not exist."
+  echo "BUCKET_EXISTS=false" >> $GITHUB_ENV
+fi
